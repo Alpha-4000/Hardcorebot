e1c9c8846a8a0d3f3ccf6b843aad8249e482f76f
Alpha-4000 <proffesorzone@gmail.com>
2026-03-26 05:34:27 +0500

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 # Kanal admin panel orqali sozlanadi, kodga qattiq yozilmaydi
 
 config = {
-    "bot_token": "",      # Bot tokenini admin paneldan qo'shadi
-    "admin_id": None,     # Admin ID
+    "8017480371:AAHqkLOjLJAPMYV4LOYGdVwJpCkydu3Vb9I": "",      # Bot tokenini admin paneldan qo'shadi
+    "5912710631": None,     # Admin ID
     "kanal_id": None      # Kanal ID
 }
