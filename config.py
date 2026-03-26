b0b36c0f673a691a210f3e70929759cf5c79a9ea
Alpha-4000 <proffesorzone@gmail.com>
2026-03-26 05:19:40 +0500

    Create config.py for bot settings
    Add configuration for bot token, admin ID, and channel ID.

---
+++ config.py
@@ -0,0 +1,8 @@
+# Bot tokeni va admin / kanal ID sini shu yerda saqlaymiz
+# Kanal admin panel orqali sozlanadi, kodga qattiq yozilmaydi
+
+config = {
+    "bot_token": "",      # Bot tokenini admin paneldan qo'shadi
+    "admin_id": None,     # Admin ID
+    "kanal_id": None      # Kanal ID
+}
